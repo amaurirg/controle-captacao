2fa6303c59e176e8595ae85bbfad59f36c8a6477
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2022-03-22 00:26:05 -0300

    dj_database_url config

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
 from decouple import config
 from dj_database_url import parse as dburl
 
@@ -88,21 +90,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.config()
-# }
-#
-# if config('DB_NAME'):
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config()
 }
+
+if config('DB_NAME'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASS'),
+            'HOST': config('DB_HOST', default='localhost'),
+            'PORT': config('DB_PORT', default='5432'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
